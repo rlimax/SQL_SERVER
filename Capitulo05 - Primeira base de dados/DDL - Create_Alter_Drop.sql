ae984dd2890c6a66b9c222e307d1b1d0500f6524
Rodrigo Lima <rodrigoetb@gmail.com>
2021-08-10 23:07:37 -0300

    DDL

--- Capitulo05 - Primeira base de dados/DDL - Create_Alter_Drop.sql
+++ Capitulo05 - Primeira base de dados/DDL - Create_Alter_Drop.sql
@@ -10,6 +10,39 @@
 CREATE DATABASE dataone;
 GO
 
+---------------------------Criar novo arquivo de dados para o banco de dados
+ALTER DATABASE dataone ADD FILE ( NAME = 'dataone_n', 
+								  FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\dataone_n.ndf' , 
+								  SIZE = 8192KB , 
+								  MAXSIZE = 102400KB , 
+								  FILEGROWTH = 10240KB ) TO FILEGROUP [PRIMARY]
+GO
+
+---------------------------Lista arquivos referente ao banco de dados
+SELECT df.file_id, df.name  from sys.database_files df
+GO
+
+---------------------------Esvaziar arquivo de dados
+DBCC SHRINKFILE ('dataone_n' , EMPTYFILE)
+GO
+
+---------------------------Remove arquivo de dados
+ALTER DATABASE dataone  REMOVE FILE dataone_n
+GO
+
+---------------------------Adicionar novo grupo de arquivos
+ALTER DATABASE dataone ADD FILEGROUP EXTRA
+GO
+
+---------------------------Criar novo arquivo de dados para o banco de dados
+ALTER DATABASE dataone ADD FILE ( NAME = 'dataone_n', 
+								  FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\dataone_n.ndf' , 
+								  SIZE = 8192KB , 
+								  MAXSIZE = 102400KB , 
+								  FILEGROWTH = 10240KB ) TO FILEGROUP EXTRA
+GO
+
+
 ---------------------------Renomear banco de dados
 ALTER DATABASE dataone MODIFY NAME = data_one
 --EXEC sp_rename 'dataone', 'data_one';
