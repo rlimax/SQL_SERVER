753643fbbb833fb924abbe5ca537ba20735dab4a
Rodrigo Lima <rodrigoetb@gmail.com>
2021-08-11 23:29:12 -0300

    DML

---
+++ Capitulo07 - Consultas/Consultas básicas.sql
@@ -0,0 +1,14 @@
+--CAPITULO 7 CONSULTAS SIMPLES
+/*
+1 - SELECT
+2 - FROM
+3 - WHERE
+4 - BETWEEN
+5 - NOT
+6 - IN
+7 - LIKE
+8 - NULL
+9 - ORDER BY
+*/
+
+--SELECT--------------------------------------------------------------------------------
