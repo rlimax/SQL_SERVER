c48d35d8da15774887c80316ffa01b72c5a98cb0
Rodrigo Lima <rodrigoetb@gmail.com>
2021-08-18 23:35:09 -0300

    Atualização

---
+++ Capitulo08 -Consultas avançadas/Consultas avançadas.sql
@@ -0,0 +1,25 @@
+--CAPITULO 8 CONSULTAS AVANÇADAS
+/*
+1 - INNER JOIN
+2 - LEFT OUTER JOIN
+3 - RIGHT OUTER JOIN
+4 - FULL OUTER JOIN
+5 - ABS
+6 - DATEADD
+7 - DATEDIFF
+8 - DATEPART
+9 - LEFT
+10 - LEN
+11 - LOWER
+12 - PI
+13 - RAND
+14 - RIGHT
+15 - ROUND
+16 - SQRT
+17 - SQUARE
+18 - UPPER
+
+*/
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
